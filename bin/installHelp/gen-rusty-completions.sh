dc3063bee08cfb558e831668c5986bd32b88391e
StaticPH <7786502+StaticPH@users.noreply.github.com>
2023-07-23 15:55:35 -0400

    Fix bin/installHelp/gen-rusty-completions.sh

--- bin/installHelp/gen-rusty-completions.sh
+++ bin/installHelp/gen-rusty-completions.sh
@@ -1,14 +1,18 @@
 #! /bin/sh
 
 # Generate bash or zsh completions for rustup and cargo commands
-if [ "$SHELL" = "/bin/bash" ] || [ "$SHELL" = "/usr/bin/bash" ]; then
+if [ "x$BASH_VERSION" != 'x' ]; then
 	mkdir -p ~/.local/share/bash-completion/completions >/dev/null 2>&1
 	rustup completions bash > ~/.local/share/bash-completion/completions/rustup # formerly these both appended for some reason
 	rustup completions bash cargo > ~/.local/share/bash-completion/completions/cargo
-elif [ "$SHELL" = "/bin/zsh" ] || [ "$SHELL" = "/usr/bin/zsh" ]; then
+elif [ "x$ZSH_VERSION" != 'x' ]; then
+	# TODO: learn the "proper" place for these to go...
 	mkdir ~/.zcompletions >/dev/null 2>&1
 	rustup completions zsh > ~/.zcompletions/_rustup
 	rustup completions zsh cargo > ~/.zcompletions/_cargo
 	printf "Add the following line to .zshrc, just prior to 'compinit':\n%s\n" "fpath+=~/.zcompletions"
 	printf "Relog or run \"exec zsh\" for the changes to be applied\n"
+else
+	echo "Current shell is not Bash or Zsh. Why not?"
+	return 1
 fi
